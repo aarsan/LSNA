fc0b78eb3dd3a5eee1feb59279b19c38ac16633e
Ahmet Arsan <aarsan@progger.com>
2012-08-21 13:42:36 -0400

    lsna stuff

--- routes/lsna.php
+++ routes/lsna.php
@@ -2,20 +2,26 @@
 
 //this redirects from root to login page
 $app->get('/', function () {
-	header("Location: /login");
-	break;
+    header("Location: /login");
+    break;
 
 });
 
 //login page
 $app->get('/login', function () {
-	include('./view/login.html');
+    include('./view/login.html');
 
 });
 
 //this is the home page
 $app->get('/home', function () {
-	include('./view/home.html');
+/*
+    ob_start();
+    include('./assets/scripts/post.js');
+    $ajax = ob_get_contents();
+    ob_end_clean();
+*/
+    include('./view/home.html');
 });
 
 $app->get('/lsna/new', function() {
@@ -24,28 +30,28 @@ $app->get('/lsna/new', function() {
 
 $app->get('/lsna/address', function () {
 
-	$nick_name = "Ahmet";
+    $nick_name = "Ahmet";
 
-	ob_start();
+    ob_start();
     include('./lsna/post.js');
     $ajax = ob_get_contents();
     ob_end_clean();
 
-	include('./lsna/form.html');
+    include('./lsna/form.html');
 
 });
 
 $app->post('/lsna/address', function () {
 
-	$key = "search :";
-	$value = $_POST['street'];
-	$value = "'$value'";
-	$left = "{";
-	$right = "}";
+    $key = "search :";
+    $value = $_POST['street'];
+    $value = "'$value'";
+    $left = "{";
+    $right = "}";
 
-	$dataset = $left. " " .$key. " " .$value. " " .$right;
+    $dataset = $left. " " .$key. " " .$value. " " .$right;
 
-	ob_start();
+    ob_start();
     include('./lsna/query.js');
     $ajax = ob_get_contents();
     ob_end_clean();
