eb6c480a35cb7cfa7a99c782cf02fb9b48807f6b
Ahmet Arsan <aarsan@progger.com>
2012-08-23 17:31:59 -0400

    updates to lsna

--- view/all_properties.php
+++ view/all_properties.php
@@ -1,17 +1,19 @@
 <h2>List of all properties:</h2>
-<div id="content">
-  <div id="label">
-  </div>
-    <div id="invitations">
-        <table id="table-1">
-            <?php foreach ($properties AS $property) : ?>
+    <table>
+    	<thead>
+    		<th>House Number</th>
+    		<th>Full Street Name</th>
+    		<th>Zip Code</th>
+    		<th></th>
+    	</thead>
+    	<tbody>
+        <?php foreach ($properties AS $property) : ?>
                 <tr>
                 	<td><?php echo $property->getNumber(); ?></td>
                     <td><?php echo $property->getStreet(); ?></td>
                     <td><?php echo $property->getZip(); ?></td>
                     <td><a class="btn" href="/properties/<?php echo $property->getPropId(); ?>/view">view</a>
                 </tr>
-            <?php endforeach; ?>
-        </table>
-    </div>
-</div>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
