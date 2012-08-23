01044ef16a3939a6dba0815c44bfcb4910ff382f
Ahmet Arsan <aarsan@progger.com>
2012-08-22 21:20:18 -0400

    updates to lsna

--- assets/scripts/street_match.js
+++ assets/scripts/street_match.js
@@ -1,3 +1,4 @@
+
                 $('#data_box').append('<h2>Pick a matching street</h2>');
 
                 $.each(response.data, function (index, value) {
@@ -6,6 +7,6 @@
                     var word = string.split(",");
                     var street = word[8];
                     var id = word[0];
-                    $('#data_box').append('<li><a href="'+id+'">'+street+'</a></li>');
+                    $('#data_box').append('<li><a href="/select/street/'+id+'">'+street+'</a></li>');
 
                     });
