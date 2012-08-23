89b4ce5aa81f73dddbb5060b497855cef8dfa3ec
Ahmet Arsan <aarsan@progger.com>
2012-08-22 23:05:18 -0400

    updates to lsna

--- assets/scripts/street_select.js
+++ assets/scripts/street_select.js
@@ -1,7 +1,10 @@
 				var dir = (response.direction);
 				var street = (response.street);
 				var suff = (response.suffix);
+				var min = (response.min_address);
+				var max = (response.max_address);
 
                 $('#data_box').html('<h2>You selected the following street:</h2>');
                 $('#data_box').append(' '+dir+' '+street+' '+suff+' ');
                 $('#data_box').append('<p>Acceptable address is in the range below:</p>');
+                $('#data_box').append(' '+min+' '+max+' ');
