73233a8609c95128e926c3f34a5c6fb3eec76e1e
Ahmet Arsan <aarsan@progger.com>
2012-08-22 22:19:24 -0400

    updates to lsna

--- assets/scripts/street_select.js
+++ assets/scripts/street_select.js
@@ -1,3 +1,5 @@
 
-                $('#data_box').html('<h2>You selected a street</h2>');
+                $('#data_box').html('<h2>You selected the following street:</h2>');
+                $('#data_box').append(response.direction);
                 $('#data_box').append(response.street);
+                $('#data_box').append(response.suffix);
