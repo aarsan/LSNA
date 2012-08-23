0731dd8f46bfb072d6e5e89f31766edfec1f2a7a
Ahmet Arsan <aarsan@progger.com>
2012-08-23 16:48:58 -0400

    updates to lsna

--- model/properties_db.php
+++ model/properties_db.php
@@ -2,14 +2,14 @@
 
 class PropertiesDB {
 	public static function newProperty($property) {
-		
+
 		$street = $property->getStreet();
 		$number = $property->getNumber();
 		$zip = $property->getZip();
 
 		$db = Database::getDB();
 		$query = "INSERT INTO properties
-					(full_street_name, house_number, zip)
+					(DEFAULT, full_street_name, house_number, zip)
 				  VALUES
 				    (:street, :number, :zip)";
 	    $statement = $db->prepare($query);
