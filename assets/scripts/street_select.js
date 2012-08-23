bb66ab25ff7e21929fbf9e82f40fe08a6add8a67
Ahmet Arsan <aarsan@progger.com>
2012-08-22 23:03:18 -0400

    updates to lsna

--- assets/scripts/street_select.js
+++ assets/scripts/street_select.js
@@ -4,3 +4,4 @@
 
                 $('#data_box').html('<h2>You selected the following street:</h2>');
                 $('#data_box').append(' '+dir+' '+street+' '+suff+' ');
+                $('#data_box').append('<p>Acceptable address is in the range below:</p>');
