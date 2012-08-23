dbd7baf8b1a784a53876d2da6481506df81bc5e1
Ahmet Arsan <aarsan@progger.com>
2012-08-23 15:37:25 -0400

    updates to lsna

--- assets/scripts/street_select.js
+++ assets/scripts/street_select.js
@@ -7,7 +7,7 @@
 
                 $('#data_box').html('<h2>You selected the following street:</h2>');
                 //$('#data_box').append(' <strong> '+dir+' '+street+' '+suff+' </strong> ');
-                $('#data_box').append(full_street_address);
+                $('#data_box').append(full_address);
                 $('#data_box').append('<p>Address must fall within the range below:</p>');
                 $('#data_box').append(' '+min+' to '+max+' ');
                 $('#data_box').append('</br><form><input type=\"text\" name=\"number\" />');
