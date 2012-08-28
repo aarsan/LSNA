0320e64cac16931a4e1b47ad45db6bab19875ac6
Ahmet Arsan <aarsan@progger.com>
2012-08-28 12:15:29 -0400

    updates

--- view/user_register.php
+++ view/user_register.php
@@ -1,1 +1,8 @@
 <h2>New User Registration Page</h2>
+<form>
+	<input type="text" name="first_name" />
+	<input type="text" name="last_name" />
+	<input type="text" name="email" />
+	<input type="password" name="password" />
+	<input type="password" name="pw_repeat" />
+</form>
