9119c20433b1cc413ce0255376e930156714a443
Ahmet Arsan <aarsan@progger.com>
2012-08-28 14:33:01 -0400

    updates

--- routes/properties.php
+++ routes/properties.php
@@ -37,7 +37,7 @@ $app->get('/properties/new', function() {
     include('./assets/scripts/post.js');
     $ajax = ob_get_contents();
     ob_end_clean();
-
+    $action = "<a href=\"/users/logout\">logout</a>";
     include('./view/new_property.php');
 
 });
