6dd51132a1152df9fa4c14b65e72a11adbe0c0c1
Ahmet Arsan <aarsan@progger.com>
2012-08-28 12:18:40 -0400

    updates

--- view/user_register.php
+++ view/user_register.php
@@ -1,8 +1,26 @@
 <h2>New User Registration Page</h2>
 <form>
-	<input type="text" name="first_name" /></br>
-	<input type="text" name="last_name" /></br>
-	<input type="text" name="email" /></br>
-	<input type="password" name="password" /></br>
-	<input type="password" name="pw_repeat" />
+	<table>
+		<tr>
+			<td>First Name:</td>
+			<td><input type="text" name="first_name" /></br></td>
+		</tr>
+		<tr>
+			<td>Last Name:</td>
+			<td><input type="text" name="last_name" /></br></td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td><input type="text" name="email" /></br></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="password" /></br></td>
+		</tr>
+		<tr>
+			<td>Re-Enter Password:</td>
+			<td><input type="password" name="pw_repeat" /></td>
+		</tr>
+
+	</table>
 </form>
