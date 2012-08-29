c2a77de8488e6a813f6d9d9caeea7a92bdb3dfa9
Ahmet Arsan <aarsan@progger.com>
2012-08-28 23:01:02 -0400

    updates

--- view/mod_property.php
+++ view/mod_property.php
@@ -1,7 +1,16 @@
 <?php include('./view/header.php'); ?>
 <h2>Please select the question you'd like to work on.</h2>
 <?php foreach ($questions AS $question) : ?>
-    <ul>
-        <li><?php echo $question->getQVerb(); ?></li>
-    </ul>
+    <table>
+    	<thead>
+    		<th>Question</th>
+    		<th>Completed?</th>
+    	</thead>
+    	<tbody>
+	        <tr>
+	        	<td><?php echo $question->getQVerb(); ?></td>
+	        	<td>No</td>
+	        </tr>
+    	</tbody>
+    </table>
 <?php endforeach; ?>
