2d3d60cb3c92ebe5b21ab923bcc130a2e79ea11d
Ahmet Arsan <aarsan@progger.com>
2012-08-29 00:25:00 -0400

    updates

--- view/question.php
+++ view/question.php
@@ -8,4 +8,4 @@
 </select>
 <input type="submit" value="Submit" />
 </form>
-<a href="/properties/modify/<?php echo $prop_id; ?>back</a>"
+<a href="/properties/modify/<?php echo $prop_id; ?>">back</a>
