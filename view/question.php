bac60afec857951d7db63f6a856de5848bd6c5c4
Ahmet Arsan <aarsan@progger.com>
2012-08-29 00:22:35 -0400

    updates

--- view/question.php
+++ view/question.php
@@ -1,4 +1,10 @@
 <?php include('./view/header.php'); ?>
 <h2>Please complete the question below.</h2>
 <p><?php echo $question; ?></p>
-<?php echo $answer_form; ?>
+<form method="post" action="/">
+<select>
+   <option value="option1"><?php echo $option1; ?></option>
+   <option value="option2"><?php echo $option2; ?></option>
+</select>
+<input type="submit" value="Submit" />
+</form>
