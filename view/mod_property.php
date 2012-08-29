8388ca4ecc19e4bcc3e5b7172683e29c7c5dd3cf
Ahmet Arsan <aarsan@progger.com>
2012-08-28 23:15:37 -0400

    updates

--- view/mod_property.php
+++ view/mod_property.php
@@ -2,14 +2,14 @@
 <h2>Please select the question you'd like to work on.</h2>
     <table>
     	<thead>
-    		<th>Question</th>
     		<th>Completed?</th>
+    		<th>Question</th>
     	</thead>
     	<tbody>
 <?php foreach ($questions AS $question) : ?>
 	        <tr>
-	        	<td><?php echo $question->getQVerb(); ?></td>
 	        	<td>No</td>
+	        	<td><?php echo $question->getQVerb(); ?></td>
 	        </tr>
 <?php endforeach; ?>
     	</tbody>
