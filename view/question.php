16794941706308fe31fb8d780283c5ff88a3a99e
Ahmet Arsan <aarsan@progger.com>
2012-08-29 01:05:42 -0400

    updates

--- view/question.php
+++ view/question.php
@@ -2,9 +2,9 @@
 <h2>Please complete the question below.</h2>
 <p><?php echo $question; ?></p>
 <form method="post" action="/properties/<?php echo $prop_id; ?>/question/<?php echo $q_id; ?>">
-<select>
-   <option name="answer_verb" value="option1"><?php echo $option1; ?></option>
-   <option name="answer_verb" value="option2"><?php echo $option2; ?></option>
+<select name="answer_verb">
+   <option value="option1"><?php echo $option1; ?></option>
+   <option value="option2"><?php echo $option2; ?></option>
 </select>
 <input type="submit" value="Submit" />
 </form>
