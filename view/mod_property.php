e6e29068a5eb09dde885d26e0d4802b0a809724e
Ahmet Arsan <aarsan@progger.com>
2012-08-30 15:58:13 -0400

    updates

--- view/mod_property.php
+++ view/mod_property.php
@@ -1,12 +1,12 @@
 <?php include('./view/header.php'); ?>
 <h2>Questionaire for 7009 N. Keeler, 60646</h2>
 <p>Click on the question to answer it.</p>
-
+    <table>
 	<?php foreach ($answered_questions AS $question) : ?>
 	        <tr>
 	        	<td><a href="/properties/<?php echo $prop_id; ?>/question/<?php echo $question->getQId(); ?>"><?php echo $question->getQVerb(); ?></a></td>
 	        </tr>
 	<?php endforeach; ?>
-
+    </table>
 </br>
 <a href="/home">home</a>
