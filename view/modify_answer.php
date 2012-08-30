38b8c303bb495ba9da4f6fd87d7279c2a9084e50
Ahmet Arsan <aarsan@progger.com>
2012-08-30 19:11:45 -0400

    updates

--- view/modify_answer.php
+++ view/modify_answer.php
@@ -2,7 +2,6 @@
 <h2>Modify your answer:</h2>
 <p><?php echo $answer['q_verb']; ?></p>
 <p>You answered: <?php echo $answer['answer_verb']; ?></p>
-</br>
 <h4>New Answer</h4>
 <form method="post" action="/properties/<?php echo $prop_id; ?>/question/<?php echo $q_id; ?>">
 <select name="answer_verb">
