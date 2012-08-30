b7e01fcf8e7c45d224aa9479d69eefeae88eebaa
Ahmet Arsan <aarsan@progger.com>
2012-08-30 15:56:34 -0400

    updates

--- view/mod_property.php
+++ view/mod_property.php
@@ -2,18 +2,11 @@
 <h2>Questionaire for 7009 N. Keeler, 60646</h2>
 <p>Click on the question to answer it.</p>
     <table>
-    	<thead>
-    		<th>Completed?</th>
-    		<th>Question</th>
-    	</thead>
-    	<tbody>
 	<?php foreach ($answered_questions AS $question) : ?>
 	        <tr>
-	        	<td>No</td>
 	        	<td><a href="/properties/<?php echo $prop_id; ?>/question/<?php echo $question->getQId(); ?>"><?php echo $question->getQVerb(); ?></a></td>
 	        </tr>
 	<?php endforeach; ?>
-    	</tbody>
     </table>
 </br>
 <a href="/home">home</a>
