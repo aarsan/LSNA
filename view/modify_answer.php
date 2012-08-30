8d1e3f86c8b4b388101ad05b61571e01337a6409
Ahmet Arsan <aarsan@progger.com>
2012-08-30 19:07:20 -0400

    updates

--- view/modify_answer.php
+++ view/modify_answer.php
@@ -1,4 +1,5 @@
 <?php include('./view/header.php'); ?>
 <h2>Modify your answer:</h2>
 <p><?php echo $answer['q_verb']; ?></p>
+<p>You answered: <?php echo $answer['answer_verb']; ?></p>
 <a href="/properties/modify/<?php echo $prop_id; ?>">back</a>
