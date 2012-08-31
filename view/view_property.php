4f385628b73440b401ca1033302426be96c320ee
Ahmet Arsan <aarsan@progger.com>
2012-08-31 12:58:37 -0400

    updates

--- view/view_property.php
+++ view/view_property.php
@@ -1,5 +1,6 @@
-<h2>Property Information</h2>
-<?php echo $property_name; ?>
+<h1>Property Information</h1>
+<h2><?php echo $property_name; ?></h2>
+<p>Below are the answers for this property</p>
 <?php foreach ($answers AS $answer) : ?>
 	<table>
 		<tr>
