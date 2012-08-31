65f682e1b9218eb0f2ac2471d8d47bc494254b88
Ahmet Arsan <aarsan@progger.com>
2012-08-31 13:05:09 -0400

    updates

--- view/view_property.php
+++ view/view_property.php
@@ -1,17 +1,17 @@
 <h1>Property Information</h1>
 <h2><?php echo $property_name; ?></h2>
 <p>Below are the answers for this property</p>
-<?php foreach ($answers AS $answer) : ?>
 	<table>
 		<thead>
 			<th>Question</th>
 			<th>Answer</th>
 		<tbody>
+	<?php foreach ($answers AS $answer) : ?>
 			<tr>
 				<td><?php echo $answer->getQuestionVerb(); ?></td>
 				<td><?php echo $answer->getAnswerVerb(); ?></td>
 			</tr>
+	<?php endforeach; ?>
 		</tbody>
 	</table>
-<?php endforeach; ?>
 <a href="/home">home</a>
