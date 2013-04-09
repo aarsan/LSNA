0351852471cbabc6bebc241be60e85a03f165e21
Ahmet Arsan <aarsan@progger.com>
2013-04-08 22:16:19 -0400

    updates

---
+++ inventory/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from inventory.models import Property, Question
+
+admin.site.register(Property)
+admin.site.register(Question)
+
--- lsna/urls.py
+++ lsna/urls.py
@@ -5,7 +5,4 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls', namespace="polls")),
-    url(r'^questions/', include('questions.urls')),
-    url(r'^home/', include('home.urls')),
 )
