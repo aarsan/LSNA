68982d6ad830c469203000c12d53cefd0d3a2658
Ahmet Arsan <aarsan@progger.com>
2013-04-20 08:02:14 -0400

    fixed url patterns

--- inventory/views.py
+++ inventory/views.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 
 def index(request):
     time = datetime.datetime.now()
-    user = User.objects.all()
-    context = {'time': time, 'user': user}
+    users = User.objects.all()
+    context = {'time': time, 'users': users}
     return render(request, 'index.html', context)
+
+def home(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'home.html', context)
+
+def login(request):
+    return render()
+    
