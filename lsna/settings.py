ada7a8ff3f665e060a3cd84647d6315d927334e0
Ahmet Arsan <aarsan@progger.com>
2013-05-18 11:29:54 -0400

    updates

--- lsna/settings.py
+++ lsna/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/aarsan/Data/Projects/Python/lsna/lsna/sqlite3.db',                      # Or path to database file if using sqlite3.
+	'NAME': 'c:\data\projects\python\lsna/sqlite3.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/aarsan/Data/Projects/Python/lsna/lsna/templates'
+    'c:\Data\Projects\Python\lsna\templates'
 )
 
 INSTALLED_APPS = (
